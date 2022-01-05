8d5b1334e1c4a54a54f24679f6df067ca0e1d348
dafinoer <dafinoer@outlook.com>
2022-01-05 22:09:36 +0700

    added tested category

--- test/thenextdev_repository_test.dart
+++ test/thenextdev_repository_test.dart
@@ -1,12 +1,23 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
 
 import 'package:thenextdev_repository/thenextdev_repository.dart';
 
+class MockCategory extends Mock implements CategoryRepository<CategoryModel> {}
+
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
+  group('Build Object Repository', () {
+    test('category', () {
+      final category = MockCategory();
+      when(() => category.getCategoryFromService()).thenAnswer(
+        (_) => Future.value(
+          const CategoryModel(id: 20, title: '123', urlImage: '12312'),
+        ),
+      );
+      expect(
+          category.getCategoryFromService(),
+          Future<CategoryModel>.value(
+              const CategoryModel(id: 20, title: '123', urlImage: '12312')));
+    });
   });
 }
